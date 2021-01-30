ec5ec8ae6cb6d64dc3ce03899e79633e159848fd
marcj <marc@marcjschmidt.de>
2021-01-30 17:55:57 +0100

    fix: write test on macOS
    in macOS the written buffers are not batched together to one, but multiple small ones (usually 2-3).

--- test/write.js
+++ test/write.js
@@ -98,40 +98,62 @@ tape('writev twice', function (t) {
   })
 })
 
-tape('write 256 buffers', function (t) {
+tape('write small buffer', function (t) {
   const server = turbo.createServer(echo)
 
   server.listen(function () {
     const client = turbo.connect(server.address().port)
-    const expected = Buffer.alloc(256)
+    const expected = Buffer.alloc(1)
 
-    read(client, 256, function (err, buf) {
+    read(client, 1, function (err, buf) {
       t.error(err, 'no error')
       t.same(buf, expected)
       server.close()
       client.close(() => t.end())
     })
 
-    for (var i = 0; i < 256; i++) {
+    expected[0] = 15
+    client.write(new Uint8Array([15]))
+  })
+})
+
+tape('write 256 buffers', function (t) {
+  const server = turbo.createServer(echo)
+
+  server.listen(function () {
+    const client = turbo.connect(server.address().port)
+    let expected = Buffer.alloc(256)
+
+    read(client, 256, function (err, buf, n) {
+      t.error(err, 'no error')
+      t.same(buf, expected)
+      server.close()
+      client.close(() => t.end())
+    })
+
+    for (let i = 0; i < 256; i++) {
       expected[i] = i
-      client.write(Buffer.from([i]))
+      client.write(new Uint8Array([i]))
     }
   })
 })
 
 function read (socket, read, cb) {
   const buf = Buffer.alloc(read)
-  socket.read(buf, function (err, next, n) {
+  function r (err, next, n) {
     if (err) return cb(err)
     read -= n
-    if (!read) return cb(null, buf)
-    socket.read(next.slice(n), cb)
-  })
+    if (!read) return cb(null, buf, read)
+    // continue reading
+    socket.read(next.slice(n), r)
+  }
+  socket.read(buf, r)
 }
 
 function echo (socket) {
   socket.read(Buffer.alloc(65536), function onread (err, buf, n) {
     if (err) return
+    console.log('echo', n, buf)
     socket.write(buf, n, function (err) {
       if (err) return
       socket.read(buf, onread)
